8ca62e6028654cda88526775c799afde3242c562
Ayoub El Aboudy <ayub.elaboudy@gmail.com>
2021-01-14 00:29:39 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int e,count=0,total=0;
+
+    do{
+    printf("Entrez un entier positif: \n");
+    scanf("%d",&e);
+    if(e > -1){
+    count++;
+    total += e;
+    }
+    }while(e!=-1);
+
+
+    printf("La moyenne de ces %d entiers %f\n",count,(float)total/count);
+    
+	return 0;
+}
